95dc6ef7ba9af7b1cedc21bcf8e3e6d4dd40d274
venushaamarathunga <avenusha95@gmail.com>
2023-07-01 22:04:49 +0530

    check some path in counters.jsx file

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,35 +3,27 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2"],
+    tags: [],
   };
+
+  renderTag() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-          <li></li>
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTag()}
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
 import Counter from "./components/counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
