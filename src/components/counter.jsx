22566ff25fcd47761c0f27cebc17e3841c206e5a
venushaamarathunga <avenusha95@gmail.com>
2023-07-01 15:54:34 +0530

    first mosh react app develop

---
+++ src/components/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2"],
+  };
+
+  render() {
+    return (
+      <div>
+        <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
